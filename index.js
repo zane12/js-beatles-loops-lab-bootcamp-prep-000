21b95c5902169e00bcde5600e87b5cf3a800ed87
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-31 07:49:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments){
+  
+  var bands = []
+  
+  for(var i = 0; i < musicians.length; i++){
+    
+    bands = [...bands, musicians[i] + " plays " + instruments[i]]
+    
+    
+  }
+  
+  return bands
+  
+}
+
+
+
+function johnLennonFacts(facts) {
+  
+  var i = facts.length
+  
+  while(i > 0){
+    facts[i] = facts[i] + "!!!"
+    i--
+  }
+  
+  return facts
+  
+}
